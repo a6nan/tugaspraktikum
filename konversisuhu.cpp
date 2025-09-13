b7174737dc393a4b1e8d9d87256a8199846e69bd
Afnan Kurniawan <afnank@Afnans-MacBook-Pro.local>
2025-09-13 22:52:02 +0700

    Konversi Suhu jadi

--- konversisuhu.cpp
+++ konversisuhu.cpp
@@ -23,46 +23,17 @@ int main(){
 
     if(menu==1)
     {
+        cout << "Suhu Awal Celcius" << endl;
         cout << "Pilih Suhu Akhir" << endl;
         cout << "1. Fahrenheit" << endl;
         cout << "2. Kelvin" << endl;
         cout << "3. Reamur" << endl;
         cout << "Pilih Menu [1/2/3]" << endl;
         cin >> menucelcius;
-    }
 
-    else if(menu==2)
+        if(menucelcius==1) //Celcius to Fahrenheit
     {
-        cout << "Pilih Suhu Akhir" << endl;
-        cout << "1. Celcius" << endl;
-        cout << "2. Kelvin" << endl;
-        cout << "3. Reamur" << endl;
-        cout << "Pilih Menu [1/2/3]" << endl;
-        cin >> menufahrenheit;
-    }
-
-        else if(menu==3)
-    {
-        cout << "Pilih Suhu Akhir" << endl;
-        cout << "1. Celcius" << endl;
-        cout << "2. Fahrenheit" << endl;
-        cout << "3. Reamur" << endl;
-        cout << "Pilih Menu [1/2/3]" << endl;
-        cin >> menukelvin;
-    }
-
-    else if(menu==4)
-    {
-        cout << "Pilih Suhu Akhir" << endl;
-        cout << "1. Celcius" << endl;
-        cout << "2. Fahrenheit" << endl;
-        cout << "3. Kelvin" << endl;
-        cout << "Pilih Menu [1/2/3]" << endl;
-        cin >> menureamur;
-    }
-
-    if(menucelcius==1) //Celcius to Fahrenheit
-    {
+        cout << "Celcius to Fahrenheit" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = (9.0/5.0 * suhuawal) + 32;
@@ -71,6 +42,7 @@ int main(){
     
     if(menucelcius==2) //Celcius to Kelvin
     {
+    cout << "Celcius to Kelvin" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = suhuawal + 273.15;
@@ -79,16 +51,27 @@ int main(){
 
     if(menucelcius==3) //Celcius to Reamur
     {
+        cout << "Celcius to Reamur" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = (suhuawal * 4) / 5;
         cout << "Suhu Akhir : " << suhuakhir << endl;    
     }
+    }
 
-   //////////////////////////////////////////////////////////////////////////////
-
-    if(menufahrenheit==1) //Fahrenheit to Celcius
+    else if(menu==2)
     {
+        cout << "Suhu Awal Fahrenheit" << endl;
+        cout << "Pilih Suhu Akhir" << endl;
+        cout << "1. Celcius" << endl;
+        cout << "2. Kelvin" << endl;
+        cout << "3. Reamur" << endl;
+        cout << "Pilih Menu [1/2/3]" << endl;
+        cin >> menufahrenheit;
+
+        if(menufahrenheit==1) //Fahrenheit to Celcius
+    {
+        cout << "Fahrenheit to Celcius" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = 5.0/9.0 * (suhuawal - 32);
@@ -97,6 +80,7 @@ int main(){
 
     if(menufahrenheit==2) //Fahrenheit to Kelvin
     {
+        cout << "Fahrenheit to Kelvin" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = 5.0/9.0 * (suhuawal - 32) + 273.15;
@@ -105,16 +89,89 @@ int main(){
 
     if(menufahrenheit==3) //Fahrenheit to Reamur
     {
+        cout << "Fahrenheit to Reamur" << endl;
         cout << "Suhu Awal : ";
         cin >> suhuawal;
         suhuakhir = 4.0/9.0 * (suhuawal - 32);
         cout << "Suhu Akhir : " << suhuakhir << endl;    
     }
+    }
+
+        else if(menu==3)
+    {
+        cout << "Suhu Awal Kelvin" << endl;
+        cout << "Pilih Suhu Akhir" << endl;
+        cout << "1. Celcius" << endl;
+        cout << "2. Fahrenheit" << endl;
+        cout << "3. Reamur" << endl;
+        cout << "Pilih Menu [1/2/3]" << endl;
+        cin >> menukelvin;
 
 
+        if(menukelvin==1) //Kelvin to Celcius
+        {
+        cout << "Kelvin to Celcius" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir = suhuawal - 273.15;
+        cout << "Suhu Akhir : " << suhuakhir << endl;    
+    }
+        if(menukelvin==2) //Kelvin to Fahrenheit
+        {
+        cout << "Kelvin to Fahrenheit" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir = 1.8 * (suhuawal - 273.15) + 32;
+        cout << "Suhu Akhir : " << suhuakhir << endl;    
+    }
 
+        if(menukelvin==3) //Kelvin to Reamur
+        {
+        cout << "Kelvin to Reamur" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir = 4.0/5.0 * (suhuawal - 273.15);
+        cout << "Suhu Akhir : " << suhuakhir << endl;    
+    }
 
+    }
 
+    else if(menu==4)
+    {
+        cout << "Suhu Awal Reamur" << endl;
+        cout << "Pilih Suhu Akhir" << endl;
+        cout << "1. Celcius" << endl;
+        cout << "2. Fahrenheit" << endl;
+        cout << "3. Kelvin" << endl;
+        cout << "Pilih Menu [1/2/3]" << endl;
+        cin >> menureamur;
     
-
+        if(menureamur==1) //Reamur to Celcius
+        {
+        cout << "Reamur to Celcius" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir = 5.0/4.0 * suhuawal;
+        cout << "Suhu Akhir : " << suhuakhir << endl;
+        }
+        
+        if(menureamur==2) //Reamur to Fahrenheit
+        {
+        cout << "Reamur to Fahrenheit" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir =  9.0/4.0 * suhuawal + 32;
+        cout << "Suhu Akhir : " << suhuakhir << endl;
+        }
+        
+        if(menureamur==3) //Reamur to Kelvin
+        {
+        cout << "Reamur to Kelvin" << endl;
+        cout << "Suhu Awal : ";
+        cin >> suhuawal;
+        suhuakhir = (suhuawal * 5.0/4.0) + 273.15;
+        cout << "Suhu Akhir : " << suhuakhir << endl;
+        }    
+    }
+    return 0;
 }
